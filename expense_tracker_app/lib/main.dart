41f5e5897549809d086f34971d31db4eeb450383
toey <toey1234567891234@gmail.com>
2025-05-14 21:31:13 +0700

    ติดตั้ง Fire base

--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:expense_tracker_app/ui/login/login_page.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'firebase_options.dart'; // Import the generated Firebase options file
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure that Flutter is initialized
+  await Firebase.initializeApp(
+    options:
+        DefaultFirebaseOptions
+            .currentPlatform, // Initialize Firebase with platform-specific options
+  );
+  runApp(const MyApp()); // Run the app after Firebase initialization
 }
 
 class MyApp extends StatefulWidget {
